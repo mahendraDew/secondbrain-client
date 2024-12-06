e4f3a15a6a747ff372cf7878f91764cdd7aa66a4
mahendra <mahendradewangan195@gmail.com>
2024-12-07 00:01:26 +0530

    styling the card in shared component

--- src/pages/SharedContent.tsx
+++ src/pages/SharedContent.tsx
@@ -76,20 +76,6 @@ export default function SharedContent () {
     fetchContent() // Fetch content when component mounts
   }, [hash]) // Dependency array ensures this runs when the hash changes
 
-  // const findTag = (id: string) => {
-  // const inpTagId = alltags.find(tag => tag._id === id)
-  // if (inpTagId) {
-  // Add to selected tags if it exists
-  //   setSelectedTags([...selectedTags, inpTagId.title])
-  // console.log('selected tag:', selectedTags)
-  // setAlltagsId([...alltagsId, existingTag._id])
-  // console.log('all tags id : ', alltagsId)
-  // } else {
-  // Add new tag to the database
-  // console.log('find tag ka else wala part')
-  // }
-  // }
-
   if (loading)
     return (
       <div className='w-full h-screen flex flex-col justify-center items-center text-sm md:text-lg '>
@@ -104,7 +90,7 @@ export default function SharedContent () {
     )
 
   return (
-    <div className='min-h-screen flex flex-col items-center mx-auto bg-transparent max-w-7xl rounded-md w-screen'>
+    <div className='min-h-screen flex flex-col items-center mx-auto bg-transparent max-w-6xl rounded-md w-screen'>
       {/* Header Section */}
       <div className='p-10 md:p-10 bg-transparent flex flex-col items-center gap-2'>
         <h1 className='text-xl md:text-3xl bg-gradient-to-tr from-purple-300/80 to-white/90 bg-clip-text text-transparent'>
@@ -134,18 +120,6 @@ const SharedCard = ({
   contents: any
   alltags: AllTagsProps[]
 }) => {
-  // contents = contents.contents
-  console.log('contents:', contents)
-  // console.log("alltags from sharedcard:", alltags);
-
-  // const getTagTitles = (tagIds: string[]): string[] => {
-  // const getTagTitles = (tagIds: string[]): any => {
-  //   console.log("tagIds: ", tagIds)
-  //   tagIds.map(id => console.log(id))
-  //   console.log("alltags: ", alltags)
-  //   const temp = tagIds.map(id => alltags.filter(tag => tag._id === id))
-  //   console.log("temp: ", temp)
-  // };
 
   const getTagTitles = (tagIds: string[]): string[] => {
     console.log('tagIds:', tagIds)
@@ -230,70 +204,75 @@ const SharedCard = ({
 
   return (
     <div className=''>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-slate-700/10 p-5 rounded-lg min-h-96'>
+      {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-slate-700/10 p-5 rounded-lg min-h-96'> */}
+      <div className='columns-1 md:columns-2 lg:columns-3 gap-1 bg-slate-700/10 w-full rounded-lg '>
         {contents.map((content: any, index: number) => {
           const IconComponent = iconMap[content.type] || FileText // Fallback to a default icon if not found
 
           return (
-            <div
-              key={index}
-              className='border border-black/[0.2] bg-gradient-to-tr from-purple-400/10 to-transparent/5 dark:border-white/[0.2] flex flex-col items-start  relative min-h-96 w-full px-5 '
-            >
-              <Icon className='absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black' />
-              <Icon className='absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black' />
-              <Icon className='absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black' />
-              <Icon className='absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black' />
+            <div className='p-3 break-inside-avoid  w-full'>
 
-              <div className='p-2  w-full'>
-                <div className='flex flex-row items-center justify-between space-y-0 pb-2 '>
-                  <div className='flex gap-2 items-center'>
-                    {/* <div className='text-sm'>{content.type}</div> */}
-                    <IconComponent className='h-4 w-4' />
-                    <div className='font-medium'>{content.title}</div>
+              <div
+                key={index}
+                className='border border-black/[0.2] bg-gradient-to-tr from-purple-400/10 to-transparent/5 dark:border-white/[0.2] flex flex-col items-start  relative  w-full px-5 '
+              >
+                <Icon className='absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black' />
+                <Icon className='absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black' />
+                <Icon className='absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black' />
+                <Icon className='absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black' />
+
+                <div className='p-2  w-full'>
+                  <div className='flex flex-row items-center justify-between space-y-0 pb-2 '>
+                    <div className='flex gap-2 items-center'>
+                      {/* <div className='text-sm'>{content.type}</div> */}
+                      <IconComponent className='h-4 w-4' />
+                      <div className='font-medium'>{content.title}</div>
+                    </div>
+                    <a
+                      href={content.link}
+                      className='h-8 w-8 flex justify-center items-center'
+                    >
+                      <MoveUpRight className='h-4 w-4 hover:text-purple-600' />
+                    </a>
                   </div>
-                  <a
-                    href={content.link}
-                    className='h-8 w-8 flex justify-center items-center'
-                  >
-                    <MoveUpRight className='h-4 w-4 hover:text-purple-600' />
-                  </a>
-                </div>
-                <Separator />
-                <div className=''>
-                  {isYoutubeVid(content.link) ? (
-                    <div className='bg-purple-300/10 rounded-sm min-h-64 p-3 mt-2 overflow-hidden'>
-                      <div className='line-clamp-[10] overflow-hidden text-ellipsis'>
-                        {content.description}
-                        <YouTubeEmbed url={content.link} />
+                  <Separator />
+                  <div className=''>
+                    {isYoutubeVid(content.link) ? (
+                      <div className='bg-purple-300/10 rounded-sm  p-3 mt-2 overflow-hidden'>
+                        <div className='line-clamp-[10] overflow-hidden text-ellipsis'>
+                          {content.description}
+                          <YouTubeEmbed url={content.link} />
+                        </div>
                       </div>
-                    </div>
-                  ) : isTweet(content.link) ? (
-                    <div>
-                      <TwitterEmbed tweetUrl={refineTweetUrl(content.link)} />
-                    </div>
-                  ) : (
-                    <div className='bg-purple-300/10 rounded-sm min-h-64 p-3 mt-2 overflow-hidden'>
-                      <div className='line-clamp-[10] overflow-hidden text-ellipsis'>
-                        {content.description}
+                    ) : isTweet(content.link) ? (
+                      <div>
+                        <TwitterEmbed tweetUrl={refineTweetUrl(content.link)} />
                       </div>
-                    </div>
-                  )}
-                </div>
-                {/* <div className='text-gray-400'>{content.tags.map((tagId:string, index:number) => <div key={index}>{tagId}</div>)}</div> */}
-                {/* <div className='text-gray-400'>{content.tags.map((tagId:string, index:number) => <div key={index}>{tagId}</div>)}</div> */}
-                <div className=' py-2  bottom-0'>
-                  <div className='text-purple-200 flex gap-1 flex-wrap'>
-                    {getTagTitles(content.tags).map(tag => (
-                      <div className=' bg-purple-700/40 px-1 text-sm rounded-lg'>
-                        {tag}
+                    ) : (
+                      <div className='bg-purple-300/10 rounded-sm  p-3 mt-2 overflow-hidden'>
+                        <div className='line-clamp-[10] overflow-hidden text-ellipsis'>
+                          {content.description}
+                        </div>
                       </div>
-                    ))}
+                    )}
                   </div>
-                  <div className='text-gray-400  flex justify-end text-sm'>
-                    {formatDate(content.date)}
+                  {/* <div className='text-gray-400'>{content.tags.map((tagId:string, index:number) => <div key={index}>{tagId}</div>)}</div> */}
+                  {/* <div className='text-gray-400'>{content.tags.map((tagId:string, index:number) => <div key={index}>{tagId}</div>)}</div> */}
+                  <div className=' py-2  bottom-0'>
+                    <div className='text-purple-200 flex gap-1 flex-wrap'>
+                      {getTagTitles(content.tags).map(tag => (
+                        <div className=' bg-purple-700/40 px-1 text-sm rounded-lg'>
+                          {tag}
+                        </div>
+                      ))}
+                    </div>
+                    <div className='text-gray-400  flex justify-end text-sm'>
+                      {formatDate(content.date)}
+                    </div>
                   </div>
                 </div>
               </div>
+
             </div>
           )
         })}
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,6 +1,6 @@
 
 export default function Test() {
   return (
-    <div>/testing</div>
+    <div>/Testing</div>
   )
 }
